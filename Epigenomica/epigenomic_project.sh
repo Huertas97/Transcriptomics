0807f84b752a6fe63da08ae6a8f5b158e4d01f33
huertas97 <ahuertasg01@gmail.com>
2020-03-05 21:57:05 +0100

    E1, E2 y E1_E2.bed con estados

--- Epigenomica/epigenomic_project.sh
+++ Epigenomica/epigenomic_project.sh
@@ -4,16 +4,11 @@ mkdir -p "resultados" # no error if existing, make parent directories as needed
 # -wb to report the original “B” feature when an overlap is found
 bedtools intersect -wb -a Monocyte1_11_Master_11_segments.bed -b Monocyte2_11_Master_11_segments.bed -sorted > ./resultados/temp_E1.aux
 # awk will select only overlapped segments belonging to E1 in both files
-# +1 para que este 1 based
-awk '{OFS="\t"; if ($4 == $8 && $4 == "E1") print $1, $2+1, $3+1}' ./resultados/temp_E1.aux > ./resultados/E1.bed
-
-# The same for E2
-bedtools intersect -wb -a Monocyte1_11_Master_11_segments.bed -b Monocyte2_11_Master_11_segments.bed -sorted > ./resultados/temp_E2.aux
-# awk will select only overlapped segments belonging to E1 in both files
-# +1 para que este 1 based
-awk '{OFS="\t"; if ($4 == $8 && $4 == "E2") print $1, $2+1, $3+1}' ./resultados/temp_E2.aux > ./resultados/E2.bed
+awk '{OFS="\t"; if ($4 == $8 && $4 == "E1" || $4 == $8 && $4 == "E2") print $1, $2, $3, $4}' ./resultados/temp_E1.aux > ./resultados/E1_E2.bed
 
 rm -r ./resultados/*.aux 
 
-# Añadimos "chr" a la columna correspondiente al cromosoma en GFF
-# awk '{if !($1 == "#") print "chr"$4}' GRCh37_latest_genomic.gff | head
+
+# comprobacion
+# grep -cE "^chr.*E1[^0-9].*E1$" temp_E1.aux 
+# grep -cE "^chr.*E2.*E2$" temp_E1.aux
