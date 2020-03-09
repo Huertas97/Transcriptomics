ad5afdc2b1284dd147af19ee850fe1865088c10e
Sara <sara@pop-os.localdomain>
2020-03-09 13:57:19 +0100

    Anotaciones y graficos incluidos en annotate_bed.R: cds, basicgenes, enhancers, cpgs...

--- Epigenomica/annotate_bed.R
+++ Epigenomica/annotate_bed.R
@@ -3,12 +3,13 @@ title_str <- 'Status'
 
 #Load BED file
 #Fichero de prueba solo con 2000 lineas del fichero de segmentos del Monocito 1.
-dm_regions = read_regions(con = 'ficheros_prueba/Monocyte1_prueba.bed', genome = 'hg19', format = 'bed',
+dm_regions = read_regions(con = 'bed_E1_filtrado_0.7/intersect_E1_f0.7.bed', genome = 'hg19', format = 'bed',
                           rename_name = title_str)
 
 # Select annotations for intersection with regions
 # Note inclusion of custom annotation, and use of shortcuts
-annots = c('hg19_basicgenes', 'hg19_cpgs', 'hg19_genes_intergenic')
+annots = c('hg19_genes_cds', 'hg19_basicgenes', 'hg19_cpgs', 'hg19_genes_intergenic',
+           'hg19_enhancers_fantom')
 # Build the annotations (a single GRanges object)
 # BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
 annotations = build_annotations(genome = 'hg19', annotations = annots)
@@ -16,6 +17,7 @@ annotations = build_annotations(genome = 'hg19', annotations = annots)
 dm_annotated = annotate_regions(
   regions = dm_regions,
   annotations = annotations,
+  minoverlap = 1L, #Overlap of annotations
   ignore.strand = TRUE,
   quiet = FALSE)
 # A GRanges object is returned
@@ -26,13 +28,27 @@ df_dm_annotated = data.frame(dm_annotated)
 # See the GRanges column of dm_annotaed expanded
 print(head(df_dm_annotated))
 
+# Randomize the input regions
+dm_random_regions = randomize_regions(
+  regions = dm_regions,
+  allow.overlaps = TRUE,
+  per.chromosome = TRUE)
+# Annotate the random regions using the same annotations as above
+# These will be used in later functions
+dm_random_annotated = annotate_regions(
+  regions = dm_random_regions,
+  annotations = annotations,
+  ignore.strand = TRUE,
+  quiet = TRUE)
+
 # Find the number of regions per annotation type
 dm_annsum = summarize_annotations(
   annotated_regions = dm_annotated,
   quiet = TRUE)
 print(dm_annsum)
+######En total 9078 segmentos anotados como CDS
 
-# Count the occurrences of classifications in the Estado_chrom
+# Count the occurrences of classifications in the Status
 # column across the annotation types.
 dm_catsum = summarize_categorical(
   annotated_regions = dm_annotated,
@@ -42,14 +58,13 @@ print(dm_catsum)
 
 #View a heatmap of regions occurring in pairs of annotations
 annots_order = c(
-  'hg19_custom_ezh2',
-  'hg19_H3K4me3_Gm12878',
   'hg19_genes_promoters',
   'hg19_genes_5UTRs',
   'hg19_genes_exons',
   'hg19_genes_introns',
   'hg19_genes_3UTRs',
-  'hg19_genes_intergenic')
+  'hg19_genes_cds',
+  'hg19_enhancers_fantom')
 dm_vs_coannotations = plot_coannotations(
   annotated_regions = dm_annotated,
   annotation_order = annots_order,
@@ -57,17 +72,53 @@ dm_vs_coannotations = plot_coannotations(
   plot_title = 'Regions in Pairs of Annotations')
 print(dm_vs_coannotations)
 
+dm_annotations_plot = plot_annotation(annotated_regions = dm_annotated,
+                annotation_order = annots_order,
+                plot_title = 'Annotations for E1',
+                x_label = 'Annotation type',
+                y_label = 'Count')
+print(dm_annotations_plot)
+
+# View the number of regions per annotation and include the annotation
+# of randomized regions
+dm_annotations_plot_wrandom = plot_annotation(
+  annotated_regions = dm_annotated,
+  annotated_random = dm_random_annotated,
+  annotation_order = annots_order,
+  plot_title = 'Annotations for E1 (with rndm.)',
+  x_label = 'Annotation type',
+  y_label = 'Count')
+print(dm_annotations_plot_wrandom)
+
+#CpGIslands
+annots_order = c(
+  'hg19_cpg_islands',
+  'hg19_cpg_shores',
+  'hg19_cpg_shelves',
+  'hg19_cpg_inter')
+dm_annotations_plot_wrandom = plot_annotation(
+  annotated_regions = dm_annotated,
+  annotated_random = dm_random_annotated,
+  annotation_order = annots_order,
+  plot_title = 'Annotations for E1 (with rndm.)',
+  x_label = 'Annotation type',
+  y_label = 'Count')
+print(dm_annotations_plot_wrandom)
+
+########################################################################################
+########################################################################################
+###########TODO ESTO PARA VARIOS ESTADOS O VARIAS CLASES CON ALGUNA CATEGORÍA###########
+########################################################################################
+########################################################################################
 # View the proportions of data classes in knownGene annotations
 # The orders for the x-axis labels.
 x_order = c(
-  'hg19_custom_ezh2',
-  'hg19_genes_1to5kb',
   'hg19_genes_promoters',
   'hg19_genes_5UTRs',
   'hg19_genes_exons',
   'hg19_genes_introns',
   'hg19_genes_3UTRs',
-  'hg19_genes_intergenic')
+  'hg19_genes_cds')
 dm_vs_kg_cat = plot_categorical(
   annotated_regions = dm_annotated, x='annot.type', fill=title_str,
   x_order = x_order, position='fill',
@@ -96,23 +147,22 @@ print(dm_vs_cpg_cat1)
 # View the counts of CpG annotations in data classes
 # The orders for the x-axis labels. This is also a subset
 # of the labels (E1, E2, E3...).
-x_order = c(
-  'E1',
-  'E2',
-  'E3')
+x_order = c()
 # The orders for the fill labels. Can also use this
 # parameter to subset annotation types to fill.
 fill_order = c(
-  'hg19_cpg_islands',
-  'hg19_cpg_shores',
-  'hg19_cpg_shelves',
-  'hg19_cpg_inter')
+  'hg19_genes_promoters',
+  'hg19_genes_5UTRs',
+  'hg19_genes_exons',
+  'hg19_genes_introns',
+  'hg19_genes_3UTRs',
+  'hg19_genes_cds')
 # Make a barplot of the data class where each bar
 # is composed of the counts of CpG annotations.
 dm_vs_cpg_cat1 = plot_categorical(
   annotated_regions = dm_annotated, x=title_str, fill='annot.type',
   x_order = x_order, fill_order = fill_order, position='stack',
-  plot_title = 'Status by CpG Annotation Counts',
+  plot_title = 'Status by Annotation Counts',
   legend_title = 'Annotations',
   x_label = title_str,
   y_label = 'Count')
