f5d126ee2416bb6d52098058a3a26042be8eacd8
Sara <sara@pop-os.localdomain>
2020-03-10 15:08:52 +0100

    Ejemplo de uso en script getFastaFromBed_MEME.sh

--- Epigenomica/getFastaFromBed_MEME.sh
+++ Epigenomica/getFastaFromBed_MEME.sh
@@ -10,4 +10,5 @@ file_out=$2
 
 bedtools getfasta -fi hg19.fa -bed $file_in -fo $file_out 
 
+#Ejemplo
 # ./getFastaFromBed_MEME.sh bed_E1_filtrado_0.7/intersect_E1_f0.7.bed bed_E1_filtrado_0.7/intersect_E1_f0.7.fasta
