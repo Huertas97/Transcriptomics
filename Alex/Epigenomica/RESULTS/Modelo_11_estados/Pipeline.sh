c0647e63d03cf87eb32f4d70ae4fcbadabe1e804
amartin97 <jandrin1297@gmail.com>
2020-03-11 21:08:48 +0100

    El fichero pipeline.sh os crea todos los ficheros, pero eliminar las carpetas de resultados antes. Está lo de Sara aplicado a mis datos en un .R

--- Alex/Epigenomica/RESULTS/Modelo_11_estados/Pipeline.sh
+++
@@ -1,7 +0,0 @@
-python3 FileCreator.py
-
-bedtools intersect -a ./Monocyte1/States_E1.bed -b ./Monocyte2/States_E1.bed > Monocytes_E1.bed
-bedtools intersect -a ./Monocyte1/States_E2.bed -b ./Monocyte2/States_E2.bed > Monocytes_E2.bed
-
-
-
